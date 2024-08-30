baad5eedd229794eda3b970a0d21631537d4575c
wayneleon1 <rurangwaleon10@gmail.com>
2024-08-30 22:21:52 +0200

    create appwrite types

---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,14 @@
+export const createUser = async (user: CreateUserParams) => {
+   //   try {
+  //     const newUser = await database.createDocument(
+  //       DATABASE_ID,
+  //       PATIENT_COLLECTION_ID,
+  //       ID.unique(),
+  //       user
+  //     );
+  //     return newUser;
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw error;
+  //   }
+};
