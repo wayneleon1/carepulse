56cbaeda338ad96edafa93d24fb513434a099596
wayneleon1 <rurangwaleon10@gmail.com>
2024-09-04 21:36:44 +0200

    fix patient vakiga

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Form } from "@/components/ui/form";
+import { Form, FormControl } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
@@ -11,6 +11,7 @@ import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
 import { createUser } from "@/lib/actions/patient.actions";
 import { FormFieldType } from "./PateintForm";
+import { RadioGroup } from "../ui/radio-group";
 
 const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
@@ -46,7 +47,7 @@ const RegisterForm = ({ user }: { user: User }) => {
         className="space-y-12 flex-1"
       >
         <section className="space-y-4">
-          <h1 className="header">Welcom 👋</h1>
+          <h1 className="header">Welcome 👋</h1>
           <p className="text-dark-700">Let us know more about yourself.</p>
         </section>
 
@@ -80,6 +81,29 @@ const RegisterForm = ({ user }: { user: User }) => {
             name="phone"
             label="Phone Number"
             placeholder="(555) 123-4567"
+          />
+        </div>
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFormField
+            control={form.control}
+            fieldType={FormFieldType.DATE_PICKER}
+            name="birthDate"
+            label="Date of Birth"
+          />
+          <CustomFormField
+            control={form.control}
+            fieldType={FormFieldType.SKELETON}
+            name="gender"
+            label=" Gender"
+            renderSkeleton={(field) => (
+              <FormControl>
+                <RadioGroup
+                  className="flex h-11 gap-6 xl:justify-between"
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                ></RadioGroup>
+              </FormControl>
+            )}
           />
         </div>
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
