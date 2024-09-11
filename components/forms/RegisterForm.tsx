d67e15249b97a259347cbce0a6d5a1ccaedeb588
wayneleon1 <rurangwaleon10@gmail.com>
2024-09-11 22:36:22 +0200

    Working on Checkbox Input

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -262,6 +262,30 @@ const RegisterForm = ({ user }: { user: User }) => {
             </FormControl>
           )}
         />
+        <section className="space-y-6">
+          <div className="mb-9 space-x-1">
+            <h2 className="sub-header">Consent and Policy</h2>
+          </div>
+        </section>
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="treatmentConsent"
+          label="I consent to the treatment"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="disclosureConsent"
+          label="I consent to disclosure of the Information"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="privacyConsent"
+          label="I consent to the privacy pocily"
+        />
+
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
