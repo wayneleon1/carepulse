e5748fefd0a69d0feb3d148cc7d5d76ac35865e3
wayneleon1 <rurangwaleon10@gmail.com>
2024-10-20 16:24:04 +0200

    complete implementation of success appointment page

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,8 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Doctors } from "@/constants";
+import { getAppointment } from "@/lib/actions/appointment.actions";
+import { formatDateTime } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-function Success() {
+const Success = async ({
+  params: { userId },
+  searchParams,
+}: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || "";
+  const appointment = await getAppointment(appointmentId);
+  const doctor = Doctors.find(
+    (doc) => doc.name === appointment.primaryPhysician
+  );
+
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
       <div className="success-img">
@@ -31,11 +44,35 @@ function Success() {
         </section>
         <section className="request-details">
           <p>Requested appointment detaits:</p>
-          <div className="flex items-center gap-3">{/* <Image /> */}</div>
+          <div className="flex items-center gap-3">
+            <Image
+              src={doctor?.image!}
+              alt="doctor"
+              width={100}
+              height={100}
+              className="size-6"
+            />
+            <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+          </div>
+          <div className="flex gap-2">
+            <Image
+              src="/assets/icons/calendar.svg"
+              alt="calendar"
+              width={24}
+              height={24}
+            />
+            <p>{formatDateTime(appointment.schedule).dateTime}</p>
+          </div>
         </section>
+        <Button variant="outline" className="shad-primary-btn" asChild>
+          <Link href={`/patients/${userId}/new-appointment`}>
+            New Appointment
+          </Link>
+        </Button>
+        <p className="copyright"> © 2024 CarePulse</p>
       </div>
     </div>
   );
-}
+};
 
 export default Success;
