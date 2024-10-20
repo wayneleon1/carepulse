46cb113dba2d9bb1a56bbabfa54262c80398cb6e
wayneleon1 <rurangwaleon10@gmail.com>
2024-10-20 15:54:42 +0200

    implement success screen

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -11,23 +11,28 @@ function Success() {
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
-            alt="Logo"
+            alt="logo"
             className="h-10 w-fit"
           />
         </Link>
-        <section className="flex flex-col items">
+        <section className="flex flex-col items-center">
           <Image
             src="/assets/gifs/success.gif"
             width={300}
             height={300}
             alt="success"
           />
+
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            Your <span className="text-green-500">appointment request</span> has
+            been successfully submitted!
+          </h2>
+          <p>We&#39;ll be in touch shortly to confrim </p>
         </section>
-        <h2 className="header mb-6 max-w-[600px] text-center">
-          your <span className="text-green-500">appointment request</span> has
-          been successfully submitted
-        </h2>
-        <p>W3ll be in touch shotry to cimcir </p>
+        <section className="request-details">
+          <p>Requested appointment detaits:</p>
+          <div className="flex items-center gap-3">{/* <Image /> */}</div>
+        </section>
       </div>
     </div>
   );
