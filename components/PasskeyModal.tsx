3fd858fd3ca142e06bceaef8b3261bd22b8f6494
wayneleon1 <rurangwaleon10@gmail.com>
2024-10-27 14:13:12 +0200

    implement the ui design of Passkey Modal

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -9,19 +9,39 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+<<<<<<< HEAD
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+=======
+} from "@/components/ui/alert-dialog";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
+>>>>>>> implement the ui design of Passkey Modal
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const PasskeyModal = () => {
   const router = useRouter();
   const [open, setOpen] = useState(true);
+<<<<<<< HEAD
+=======
+  const [passkey, setPasskey] = useState("");
+  const [error, setError] = useState("");
+>>>>>>> implement the ui design of Passkey Modal
 
   const closeModal = () => {
     setOpen(false);
     router.push("/");
   };
+<<<<<<< HEAD
+=======
+
+  const validatePasskey = (e) => {};
+>>>>>>> implement the ui design of Passkey Modal
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
       <AlertDialogContent className="shad-alert-dialog">
@@ -41,10 +61,41 @@ const PasskeyModal = () => {
             To access the admin dashboard, please enter your passkey.
           </AlertDialogDescription>
         </AlertDialogHeader>
+<<<<<<< HEAD
         <div> </div>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
           <AlertDialogAction>Continue</AlertDialogAction>
+=======
+        <div>
+          <InputOTP
+            maxLength={6}
+            value={passkey}
+            onChange={(value) => setPasskey(value)}
+          >
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot className="shad-otp-slot" index={0} />
+              <InputOTPSlot className="shad-otp-slot" index={1} />
+              <InputOTPSlot className="shad-otp-slot" index={2} />
+              <InputOTPSlot className="shad-otp-slot" index={3} />
+              <InputOTPSlot className="shad-otp-slot" index={4} />
+              <InputOTPSlot className="shad-otp-slot" index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+          {error && (
+            <p className="shad-error text-14-regular mt-4 flex justify-center">
+              {error}
+            </p>
+          )}
+        </div>
+        <AlertDialogFooter>
+          <AlertDialogAction
+            onClick={(e) => validatePasskey(e)}
+            className="shad-primary-btn w-full"
+          >
+            Enter Admin Passkey
+          </AlertDialogAction>
+>>>>>>> implement the ui design of Passkey Modal
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
